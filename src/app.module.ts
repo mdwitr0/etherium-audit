0c933e6d55b286f88dee568bb8ef339d75c1524a
Eugene Fedoryachenko <mdwit0r@yandex.ru>
2022-01-14 17:46:18 +0300

    import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AddressModule } from './modules/address/address.module';
+import { ProxyModule } from './modules/proxy/proxy.module';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getQueueConfig } from './configs/queue.config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
+  imports: [
+    AddressModule,
+    ProxyModule,
+    BullModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getQueueConfig,
+    }),
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
